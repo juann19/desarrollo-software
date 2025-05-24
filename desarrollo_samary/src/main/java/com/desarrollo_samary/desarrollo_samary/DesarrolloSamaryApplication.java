169413bed21ff14fcccbf26cefc03a938acc89f8
juann19 <juandavidsalcedo123@gmail.com>
2025-05-24 08:44:51 -0500

    Actualización del backend con últimos cambios locales

---
+++ desarrollo_samary/src/main/java/com/desarrollo_samary/desarrollo_samary/DesarrolloSamaryApplication.java
@@ -0,0 +1,13 @@
+package com.desarrollo_samary.desarrollo_samary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarrolloSamaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesarrolloSamaryApplication.class, args);
+	}
+
+}
---
+++ desarrollo_samary/src/test/java/com/desarrollo_samary/desarrollo_samary/DesarrolloSamaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrollo_samary.desarrollo_samary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloSamaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
